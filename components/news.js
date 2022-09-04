1f063c2318bb1507ba7b540d2c9a7865b58eb664
Abderrahmane Tamazouzt <75092307+abderrahmane4dev@users.noreply.github.com>
2022-09-04 22:20:20 +0100

    v

--- components/news.js
+++ components/news.js
@@ -5,7 +5,7 @@ import img3 from "../assets/pictures/news_img3.jpg"
 function News() {
     return (
       <>
-        <div className="px-9 lg:px-24 font-[Poppins]"> 
+        <div className="px-9 lg:px-24 "> 
         <div> 
         <div className='border-b-4 my-16 border-yellow-400'>
          <h2 className='text-3xl font-bold mt-9 mb-4'> Featured News</h2>
--- components/newsletter.js
+++ components/newsletter.js
@@ -1,9 +1,17 @@
 function Newsletter() {
     return (
       <>
-        <h2> 
-            hello im a newsletter section  
-        </h2>
+        <div className="lg:px-24 px-9 mt-24 lg:flex "> 
+            <div className="px-5 text-center lg:w-1/2"> 
+              <h1 className=" md:py-0 py-4 text-3xl font-bold "> 
+                Subscribe to our newsletter
+              </h1>
+            </div>  
+            <div className="flex px-9 lg:w-1/2 flex-col"> 
+                  <input className="w-full border-2 border-black py-4 px-5 place-self-end rounded-full" type="email" placeholder="Enter your email" />
+                  <button className="mt-3 rounded-full right place-self-end text-white font-normal py-3  w-1/4 bg-sky-900"> Subscribe</button>
+            </div>
+        </div>
       </>
     );
   }
